cf6d43d57ee79b9f64b334fedd1e1e135e4784cf
victor Santos Uceta <victor.uceta@gmail.com>
2016-05-24 16:56:04 -0400

    - Continue renaming to Trueno.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import thruster from '../lib';
 
-describe('thruster', function () {
+describe('trueno', function () {
   it('should have unit test!', function () {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
