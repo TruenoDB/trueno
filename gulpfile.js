4dba44e651372597d69d92ea8fdad10e09c42825
victor Santos Uceta <victor.uceta@gmail.com>
2016-09-18 18:10:13 -0400

    -remove gulp file parameters.  -remove prebuilt from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -65,5 +65,5 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', [/*'nsp',*/ 'babel']);
+//gulp.task('prepublish', [/*'nsp',*/ 'babel']);
 gulp.task('default', ['static', 'test']);
